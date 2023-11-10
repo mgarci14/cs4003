a48440601cf8fa12720a09744e3ed25338478e13
Ben Cobb <105462919+moltenpanther@users.noreply.github.com>
2023-11-10 07:36:05 -0600

    Update connection.php
    Updated the format and added the fields needed to connect here. This is so the connection info can be centralized and easily changed

--- php/system/connection.php
+++ php/system/connection.php
@@ -1,26 +1,25 @@
 <?php
-require_once 'config.php';
 
 class Connection {
 
-  public $host;
-  public $port;
-  public $dbname;
-  public $user;
-  public $pass;
+  public $dbHost = "localhost";
+  public $dbPort = "5432";
+  public $dbname = "SEDB";
+  public $dbUser = "username";
+  public $dbPass = "password";
   
 	public static function createConnection() {
     
-    try {
-      
-      $pdo = new PDO("pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}", $this->user, $this->pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-      return $pdo;
-      
+		try {
+			
+			$pdo = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+			return $pdo;
+			
 		} catch (PDOException $e) {
 			echo "Database connection failed: " . $e->getMessage();
 			die();
-	  }
-    
+		}
+		
 	}
   
 }
